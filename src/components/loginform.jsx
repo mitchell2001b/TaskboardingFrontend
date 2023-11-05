294bd74a593d0826ea3e1c59b963ecd580d235d9
mitchell2001b <79633852+mitchell2001b@users.noreply.github.com>
2023-11-05 21:17:10 +0100

    kkkk

--- src/components/loginform.jsx
+++ src/components/loginform.jsx
@@ -17,7 +17,7 @@ function LoginForm()
       };
 
       axios
-      .post('http://api-gateway:8585/login', givenData, {
+      .post('http://172.22.0.11:8585/login', givenData, {
         headers: {
           'Content-Type': 'application/json',
         },
