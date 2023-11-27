55d67074e6d0b0ad905d94b4204e0b66e2c65391
mitchell2001b <79633852+mitchell2001b@users.noreply.github.com>
2023-11-27 09:12:29 +0100

    updated some tests

--- cypress/e2e/projectform-spec.cy.js
+++ cypress/e2e/projectform-spec.cy.js
@@ -2,6 +2,8 @@ describe('Project Form End-to-End Test', () => {
   it('should create a new project successfully', () => {
     cy.visit('http://localhost:3001/project/create');
 
+    // Intercept the HTTP request and respond with a known message
+    cy.intercept('POST', 'http://localhost:8585/newproject').as('postRequest');
     
     cy.window().then((win) => {
       cy.spy(win.console, 'log').as('consoleLog');
@@ -16,16 +18,11 @@ describe('Project Form End-to-End Test', () => {
     // Submit the form
     cy.get('button[type="submit"]').click();
 
-    // Intercept the HTTP request and respond with a known message
-    cy.intercept('POST', 'http://localhost:8585/newproject').as('postRequest');
-
+    // Check for the correct response body
     cy.wait('@postRequest').its('response').then((response) => {
-      // Now you can assert on the response body
       expect(response.body).to.deep.equal("Project created successfully");
+      cy.url().should('include', '/profile');
     });
-
-    // Assert that the redirection to '/profile' happened
-    cy.url().should('include', '/profile');
 
   });
 });
