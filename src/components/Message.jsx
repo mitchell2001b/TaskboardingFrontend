c40efd0240211f88290e7de51ab3b81d3f7742e6
mitchell2001b <79633852+mitchell2001b@users.noreply.github.com>
2023-12-08 20:29:33 +0100

    added deployment workflow

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -14,7 +14,7 @@ function Message()
     return (
         <>
             <h1>Hello semester 6</h1>
-            {1 === 2 ? <p>test test test sssssssssssssssssssssss vooorrrrrr nieuwe workflowsss</p> : null}
+            {1 === 2 ? <p>deployment test</p> : null}
             
         
         </>
